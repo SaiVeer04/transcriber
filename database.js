68070e431e606e646621f7da1890ed892d4077bc
Masterdust12 <48286570+Masterdust12@users.noreply.github.com>
2019-03-12 08:59:32 -0400

    Update database.js

--- database.js
+++ database.js
@@ -17,14 +17,8 @@ function onSignIn(googleUser) {
           //console.log("ID Token: " + id_token);
 }
 
-const app = firebase.initializeApp({
-  // use your main config
-  databaseUrl: "https://console.firebase.google.com/project/html5project-870df/database/firestore/data/"
+var database = firebase.database;
+
+firebase.database().ref('Notes_Database/Test').set({
+   user: "Me!"
 });
-
-// This is the default DB.
-const db1 = app.database();
-
-// Reference the second DB instance.
-// Keep in mind that you need to upgrade to the latest release before this will work!
-const db2 = app.database("https://multi-db501c7.firebaseio.com/");
