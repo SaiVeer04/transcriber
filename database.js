3ca68a6dbb87dbf4baebb3606abe9504e399ca52
Masterdust12 <48286570+Masterdust12@users.noreply.github.com>
2019-03-12 08:45:50 -0400

    Update database.js

--- database.js
+++ database.js
@@ -1,3 +1,21 @@
+function onSignIn(googleUser) {
+          // Useful data for your client-side scripts:
+          var profile = googleUser.getBasicProfile();
+          var userID = profile.getId();
+          console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+          //console.log('Full Name: ' + profile.getName());
+          //console.log('Given Name: ' + profile.getGivenName());
+          //console.log('Family Name: ' + profile.getFamilyName());
+          //console.log("Image URL: " + profile.getImageUrl());
+          //console.log("Email: " + profile.getEmail());
+
+          // The ID token you need to pass to your backend:
+          //var id_token = googleUser.getAuthResponse().id_token;
+          //console.log("ID Token: " + id_token);
+          
+          
+}
+
 const app = firebase.initializeApp({
   // use your main config
   databaseUrl: "https://console.firebase.google.com/project/html5project-870df/database/firestore/data/"
