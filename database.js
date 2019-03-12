24bd89947e29e3a0a03c33598b9eed9583c880a3
Masterdust12 <48286570+Masterdust12@users.noreply.github.com>
2019-03-12 08:44:14 -0400

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,11 @@
+const app = firebase.initializeApp({
+  // use your main config
+  databaseUrl: "https://console.firebase.google.com/project/html5project-870df/database/firestore/data/"
+});
 
+// This is the default DB.
+const db1 = app.database();
+
+// Reference the second DB instance.
+// Keep in mind that you need to upgrade to the latest release before this will work!
+const db2 = app.database("https://multi-db501c7.firebaseio.com/");
