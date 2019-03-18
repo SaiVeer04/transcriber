f044785e0aba532f6fc43d6d928882619b82c2b0
Masterdust12 <48286570+Masterdust12@users.noreply.github.com>
2019-03-18 08:57:47 -0400

    Update database.js

--- database.js
+++ database.js
@@ -2,23 +2,24 @@ var profile;
 var userID;
 // Get a reference to the database service
 var database = firebase.database();
+var users = null;
 
 function onSignIn(googleUser) {
-          // Useful data for your client-side scripts:
-          profile = googleUser.getBasicProfile();
-	  var id_token = googleUser.getAuthResponse().id_token;
-	  var name = profile.getGivenName();
-	  var email = profile.getEmail();
-          //console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-          //console.log('Full Name: ' + profile.getName());
-          //console.log('Given Name: ' + profile.getGivenName());
-          //console.log('Family Name: ' + profile.getFamilyName());
-          //console.log("Image URL: " + profile.getImageUrl());
-          //console.log("Email: " + profile.getEmail());
+        // Useful data for your client-side scripts:
+        profile = googleUser.getBasicProfile();
+	var id_token = googleUser.getAuthResponse().id_token;
+        var name = profile.getGivenName();
+	var email = profile.getEmail();
+        //console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        //console.log('Full Name: ' + profile.getName());
+        //console.log('Given Name: ' + profile.getGivenName());
+        //console.log('Family Name: ' + profile.getFamilyName());
+        //console.log("Image URL: " + profile.getImageUrl());
+        //console.log("Email: " + profile.getEmail());
 
-          // The ID token you need to pass to your backend:
-          //var id_token = googleUser.getAuthResponse().id_token;
-	var users = firebase.database().ref("users/");
+        // The ID token you need to pass to your backend:
+        //var id_token = googleUser.getAuthResponse().id_token;
+	users = firebase.database().ref("users/");
 
 	users.set ({
 	   [name]: {
