7044c7e394d0de765712ed75f1bbcd3ac36ce9da
SaiVeer04 <43910229+SaiVeer04@users.noreply.github.com>
2019-03-25 19:50:06 -0400

    Update database.js

--- database.js
+++ database.js
@@ -57,11 +57,11 @@ function onSignIn(googleUser) {
 	});
 	user
 }
-function read(){
+
 var ref = firebase.database().ref().child("users");
 
 ref.on("child_added", snap => {
 	alert (snap.val());
    
 });
-}):
+
