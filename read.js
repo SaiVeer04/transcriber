0a28390463f3eca9fba20333788a324588d32e32
Masterdust12 <48286570+Masterdust12@users.noreply.github.com>
2019-03-26 08:33:06 -0400

    Update read.js

--- read.js
+++ read.js
@@ -1,8 +1,34 @@
+function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+        profile = googleUser.getBasicProfile();
+	      id_token = googleUser.getAuthResponse().id_token;
+        givenname = profile.getGivenName();
+        email = profile.getEmail();
+        id = profile.getId();
+        id = "id: " + id;
 
-  var ref = firebase.database().ref().child(id);
+              //console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+              //console.log('Full Name: ' + profile.getName());
+              //console.log('Given Name: ' + profile.getGivenName());
+              //console.log('Family Name: ' + profile.getFamilyName());
+              //console.log("Image URL: " + profile.getImageUrl());
+              //console.log("Email: " + profile.getEmail());
 
-  ref.on("child_added", snap => {
-  var name = snap.child("0").val();
-  console.log(id);
-  });
- 
+              // The ID token you need to pass to your backend:
+              //var id_token = googleUser.getAuthResponse().id_token;
+        users = firebase.database().ref("users/");
+
+        users.update({
+           [id]: {
+                    id_token: [id_token],
+              user: givenname,
+              trans: [givenname]
+           }
+        });
+        var ref = firebase.database().ref().child(id);
+
+        ref.on("child_added", snap => {
+        var name = snap.child("0").val();
+        console.log(id);
+        });
+}
