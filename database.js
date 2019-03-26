78cccc0aed97a6673a138b1a1932905bd9f19b7f
Masterdust12 <48286570+Masterdust12@users.noreply.github.com>
2019-03-26 08:25:06 -0400

    Update database.js

--- database.js
+++ database.js
@@ -54,7 +54,7 @@ function onSignIn(googleUser) {
         //var id_token = googleUser.getAuthResponse().id_token;
 	users = firebase.database().ref("users/");
 
-	users.set({
+	users.update({
 	   [id]: {
               id_token: [id_token],
 	      user: givenname
