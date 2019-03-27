582be10328da6675dd0c480179fd7ca6ced26859
SaiVeer04 <43910229+SaiVeer04@users.noreply.github.com>
2019-03-27 08:57:56 -0400

    the power of trans did not work

--- read.js
+++ read.js
@@ -18,8 +18,8 @@ var Rootref = database.ref().child("users");
 Rootref.on("child_added", snap => { 
 var test = "test5765";	
 
-var name = snap.child("trans").val;
-console.log(snap.child("trans").val);
+var name = snap.child("user").val;
+console.log(snap.child("user").val);
 $("#read").append("<tr><td>"+name+"</td></tr>");
 
 
