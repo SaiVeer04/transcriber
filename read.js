b9f720735af70746206050685fcd7bac6a71128f
SaiVeer04 <43910229+SaiVeer04@users.noreply.github.com>
2019-03-27 06:51:57 -0400

    Update read.js

--- read.js
+++ read.js
@@ -1,6 +1,14 @@
+var config = {
+        apiKey: "AIzaSyB9p1VvVfhnbrcDwUKUuSqw9aQsqnDi4nQ",
+        authDomain: "html5project-870df.firebaseapp.com",
+        databaseURL: "https://html5project-870df.firebaseio.com",
+        projectId: "html5project-870df",
+        storageBucket: "html5project-870df.appspot.com",
+        messagingSenderId: "54935462861"
+      };
+    firebase.initializeApp(config);
 
-
-
+var database = firebase.database();
 var ref = firebase.database().ref().child("users");
 	
 ref.on('value', (snapshot) => { 
