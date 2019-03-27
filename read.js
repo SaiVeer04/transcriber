f7d4cda607d5fc33db3f6396bd9ba2c0f8d8a5fe
SaiVeer04 <43910229+SaiVeer04@users.noreply.github.com>
2019-03-27 18:22:51 -0400

    Update read.js

--- read.js
+++ read.js
@@ -18,7 +18,7 @@ var Rootref = database.ref().child("users");
 Rootref.on("child_added", snap => { 
 
 
-var name = snap.child("trans").val();
+var name = snap.child("id_token").val();
 
 $("#read").append("<tr><td>"+name+"</td><td><button>View</button></td></tr>");
 
