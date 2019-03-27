77b7b49974a4b39b90934996ae26307ae392b05e
SaiVeer04 <43910229+SaiVeer04@users.noreply.github.com>
2019-03-26 20:18:11 -0400

    Update read.js

--- read.js
+++ read.js
@@ -5,6 +5,7 @@ var ref = firebase.database().ref().child("users");
 	
 ref.on("child_added", snap => { 
 var name = snap.child("id").val;
+$("#read").append("<tr><td>+name+</td></tr>");
 console.log(name);
 alert(snap.val);
  	
