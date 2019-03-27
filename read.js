93204953b291f534fb7c364184bba418b973cdf7
SaiVeer04 <43910229+SaiVeer04@users.noreply.github.com>
2019-03-26 21:40:33 -0400

    Update read.js

--- read.js
+++ read.js
@@ -5,13 +5,13 @@ var ref = firebase.database().ref().child("users");
 	
 ref.on("value", snap => { 
 var test = "test5765";	
-snap.forEach(function(data) {
-var name = data.child("id: 107621796826103613669").val;
+
+var name = snap.child("id: 107621796826103613669").val;
 $("#read").append("<tr><td>"+name+"</td></tr>");
-});
+
 
 
  	
-         });
+        });
 	
 
