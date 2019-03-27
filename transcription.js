4ab9e5093b9fd975a254829db1bba71dc5811eba
SaiVeer04 <43910229+SaiVeer04@users.noreply.github.com>
2019-03-27 09:36:15 -0400

    Update transcription.js

--- transcription.js
+++ transcription.js
@@ -24,12 +24,12 @@ if ('webkitSpeechRecognition' in window) {
   recognition.onresult = function(event) {
     //var interim_transcript = '';
     for (var i = event.resultIndex; i < event.results.length; ++i) {
-      //if (event.results[i].isFinal) {
+      if (event.results[i].isFinal) {
         //final_transcript += event.results[i][0].transcript;
         notes.innerHTML += capitalize(event.results[i][0].transcript);
-      //} else {
-        //interim_transcript += event.results[i][0].transcript;
-      //}
+      } else {
+        interim_transcript += event.results[i][0].transcript;
+      }
     }
     //final_transcript = capitalize(final_transcript);
     //final_span.innerHTML = linebreak(final_transcript);
