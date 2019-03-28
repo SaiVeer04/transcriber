7072f6ee5182c81f47ad0c1fa96b9d482febcbda
SaiVeer04 <43910229+SaiVeer04@users.noreply.github.com>
2019-03-27 23:49:32 -0400

    Update database.js

--- database.js
+++ database.js
@@ -17,7 +17,7 @@ save.onclick = function() {
 	var title = titlearea.value;
 	if (database != null && users != null && !titlearea.value.includes("/") && /\S/.test(title)) {
 		var textValue = textarea.value;
-		firebase.database().ref("users/" + id ).child("Titles").update ({
+		firebase.database().ref("users/" + id ).child["Titles"].update ({
 			
 			[titlearea.value]: [textValue]
 		
