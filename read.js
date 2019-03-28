5d1dc123715246fb5d0d5b6b79529a21a4b18820
SaiVeer04 <43910229+SaiVeer04@users.noreply.github.com>
2019-03-28 06:27:02 -0400

    Update read.js

--- read.js
+++ read.js
@@ -1,5 +1,6 @@
 //var transcript_title = null;
 //var transcript = null;
+//This is to intialize everything and auth. with the firebase server
 var config = {
         apiKey: "AIzaSyB9p1VvVfhnbrcDwUKUuSqw9aQsqnDi4nQ",
         authDomain: "html5project-870df.firebaseapp.com",
@@ -9,29 +10,30 @@ var config = {
         messagingSenderId: "54935462861"
       };
 //firebase.initializeApp(config);
+//checks if it has been init
 if (!firebase.apps.length) {
     firebase.initializeApp({});
 }
+//declare variables
 var database = firebase.database();
-	
+//tells where the items are going to be	
 var Rootref = database.ref().child("users");
-var Rootref1 = database.ref().child("users").child("id: 113295907411766134791")/*.child("trans")*/;
-	
+//var Rootref1 = database.ref().child("users").child("id: 113295907411766134791")/*.child("trans")*/;
+
+//used to retrieve data	
 Rootref.on("child_added", snap => { 
+	//gets the child of titles stores it as variable
+	var transcript_title = snap.child("titles").val();
+	//gets the actual title and stores it as a var
+	var transcript = snap.child(transcript_title).val();
+	//jquery - way to add html elemnts with javascript
+	$("#read").append('<tr><td>'+transcript_title+'</td><td><button id = "clicked">View</button></td></tr>');
+	//when button view was clicked it will show the transcription
+	$("#clicked").click(function(){  
+        	$("p").append(" <b>"+transcript+"</b>.");  
+    	});  
 
-
-
-var transcript_title = snap.child("titles").val();
-
-var transcript = snap.child(transcript_title).val();
-$("#read").append('<tr><td>'+transcript_title+'</td><td><button id = "clicked">View</button></td></tr>');
-
-$("#clicked").click(function(){  
-        $("p").append(" <b>"+transcript+"</b>.");  
-    });  
-
- 	
-        });
+ });
 
 	
 
