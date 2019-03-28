217d706067c09ca58d32979e27dfdb57c3296852
SaiVeer04 <43910229+SaiVeer04@users.noreply.github.com>
2019-03-27 23:32:38 -0400

    Update read.js

--- read.js
+++ read.js
@@ -21,7 +21,7 @@ Rootref.on("child_added", snap => {
 
 
 alert(snap.val());
-var transcript_title = snap.child("user").val();
+var transcript_title = snap.child("Whatever").val();
 
 var transcript = snap.child(transcript_title).val();
 $("#read").append('<tr><td>'+transcript_title+'</td><td><button id = "clicked">View</button></td></tr>');
