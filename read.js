f3a47bd6ad08b0b3b0b397ca83560346836994a8
SaiVeer04 <43910229+SaiVeer04@users.noreply.github.com>
2019-03-28 06:49:09 -0400

    Update read.js

--- read.js
+++ read.js
@@ -28,8 +28,9 @@ Rootref.on("child_added", snap => {
 	var transcript = snap.child(transcript_title).val();
 	//jquery - way to add html elemnts with javascript
 	//$("#read").append('<tr><td>'+transcript_title+'</td><td><button id = "clicked">View</button></td></tr>');
-	$("#read").append('<option id = "work" value="'+transcript+'">'+transcript_title+'</option>')
+	$("#read1").append('<option id = "work" value="'+transcript+'">'+transcript_title+'</option>')
 	//when button view was clicked it will show the transcription
+	$(" p:selected").text();
 	//$("#clicked").click(function(){  
         	//$("p").append(" <b>"+transcript+"</b>.");  
     	//});  
