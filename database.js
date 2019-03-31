2b5b1eb34273e520c05179cd5a30d00ec2614061
SaiVeer04 <43910229+SaiVeer04@users.noreply.github.com>
2019-03-31 10:56:04 -0400

    Update database.js

--- database.js
+++ database.js
@@ -19,7 +19,7 @@ save.onclick = function() {
 	var title = titlearea.value;
 	if (database != null && users != null && !titlearea.value.includes("/") && /\S/.test(title)) {
 		var textValue = textarea.value;
-		firebase.database().ref("users/" + id +"/hi").update ({
+		firebase.database().ref("users/" + id +"/hi").set ({
 			
 			
 			[num = num + 1]:titlearea.value,
