b45236066b6c392ab5630461b1384edbfe5c6c71
Masterdust12 <48286570+Masterdust12@users.noreply.github.com>
2019-03-31 22:19:34 -0400

    Update database.js

--- database.js
+++ database.js
@@ -66,7 +66,7 @@ function Add(snapshot) {
         var ddl = document.getElementById("selectTest");
         var option = document.createElement("OPTION");
         var title = snapshot.ref.key;
-	var value = snapshot.ref.value;
+	var value = snapshot.ref.text;
 	
 	console.log("Title: " + title);
 	console.log("Value: " + value);
